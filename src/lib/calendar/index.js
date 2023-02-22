47ad90c98dec49f57a4eb5f47cc98d5a2e8bb270
wlankasper <asu.artur.smirnov@gmail.com>
2023-02-22 18:42:40 +0100

    beta calendar

---
+++ src/lib/calendar/index.js
@@ -0,0 +1,40 @@
+class CalendarBox {
+    constructor(parentElement, ...props) {
+        this.parentElement = parentElement;
+        this.rootElement;
+
+        this.props = {
+            ...props,
+        };
+    }
+
+    init() {
+
+    }
+
+    render() {
+
+    }
+
+    refresh() {
+
+    }
+
+    destroy() {
+
+    }
+
+    initTemplate() {
+
+    }
+
+    initElements() {
+
+    }
+
+    initEventListeners() {
+
+    }
+}
+
+export default CalendarBox;
